864b77c281bb503dc5ec3e76fe8e86cb872392ff
Stephane MBA <stephane.mba@netgem.com>
2024-04-24 10:09:13 +0200

    Initial Commit

---
+++ Tests/ConnectSDK-iOS-PackageTests/ConnectSDK_iOS_PackageTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import ConnectSDK_iOS_Package
+
+final class ConnectSDK_iOS_PackageTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
