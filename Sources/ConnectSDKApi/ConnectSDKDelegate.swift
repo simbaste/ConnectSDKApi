254491b2175e7b934ec78a2fb98e08699be4b36a
Stephane MBA <stephane.mba@netgem.com>
2024-04-24 12:00:07 +0200

    Initial commit

---
+++ Sources/ConnectSDKApi/ConnectSDKDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Stephane SIMO MBA on 24/04/2024.
+//
+
+import Foundation
+import LGCast
+
+public protocol ConnectSDKDelegate: AnyObject {
+//    func didFindDevices(_ devices: [ConnectableDevice])
+//    func didFailWithError(_ error: Error)
+//    func device(didConnected device: ConnectableDevice)
+//    func device(didDisconnected device: ConnectableDevice, withError error: Error)
+}
