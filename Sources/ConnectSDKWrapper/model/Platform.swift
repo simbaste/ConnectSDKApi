e008c0aaa78b50f329d38a33ab2f17de619a9522
Stephane MBA <stephane.mba@netgem.com>
2024-07-08 14:01:39 +0200

    Updated

--- Sources/ConnectSDKWrapper/model/Platform.swift
+++ Sources/ConnectSDKWrapper/model/Platform.swift
@@ -13,5 +13,5 @@ public enum Platform: String {
     case netcast = "NetcastTVService"
     case ruku = "RokuService"
     case webos = "WebOSTVService"
-    case smartview = "SmartView"
+    case smartview = "SmartViewService"
 }
